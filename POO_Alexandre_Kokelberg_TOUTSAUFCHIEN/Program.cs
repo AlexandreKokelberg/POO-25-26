c5855ca3161471cf6a0824a5bd3098239f5411af
AlexandreKokelberg <240539@site.asty-moulin.be>
2025-09-29 10:28:48 +0200

    ajout exercise chien

---
+++ POO_Alexandre_Kokelberg_TOUTSAUFCHIEN/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace POO_Alexandre_Kokelberg_TOUTSAUFCHIEN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            chien monChien = new chien("bouvier bernois", "poil", 80, 40, false, true, "TY7TG74T78978R384", 2);
+            Console.WriteLine(monChien.Manger("paté"));
+            Console.WriteLine(monChien.Dormir(12096008888885475675));
+            Console.WriteLine(monChien.AfficherCaracteristiques());
+
+            chien[] mesChines = new chien[3];
+            mesChines[0] = new chien("Malamute de l'Alaska", "magnifique", 94, 80, false, true, "TY7TG74T78978R385", 15);
+            mesChines[1] = new chien("Beagle", "fait de poil poilu", 2, -2, false, false, "TZR427HR4", 1);
+            mesChines[2] = new chien("Griffon Bruxellois", "Batiment", 16800, 4174, true, true, "84HF0EDIIHS88388SSIDHHZ9ZIRO38RYHFFGHUU8EYF52U9D9D9I3JYFUFVYH7RGF3FR77RF38RTF7ERRS58WQO9AQFR99XDL90MX0PXOJURCBNVNVVWFAEWCQZXAFRXNZUYUZABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789FBUEBVUBGE7RGR7EGRBDSH", 173);
+            for(int iChien = 0; iChien < mesChines.Length; iChien++)
+            {
+                Console.WriteLine("\n" + mesChines[iChien].AfficherCaracteristiques());
+                Console.WriteLine(mesChines[iChien].Dormir(iChien * 1074));
+            }
+        }
+    }
+}
---
+++ POO_Alexandre_Kokelberg_TOUTSAUFCHIEN/chien.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Alexandre_Kokelberg_TOUTSAUFCHIEN
+{
+    internal class chien
+    {
+        private string _race;
+        private string _pelage;
+        private double _taille;
+        private double _poids;
+        private bool _agressivite;
+        private bool _sociabilite;
+        private string _tag;
+        private double _age;
+        public chien(string race, string pelage, double taille, double poids, bool agressivite, bool sociabilite, string tag, double age)
+        {
+            _race = race;
+            _pelage = pelage;
+            _taille = taille;
+            _poids = poids;
+            _agressivite = agressivite;
+            _sociabilite = sociabilite;
+            _tag = tag;
+            _age = age;
+        }
+        public string Manger(string nouriture)
+        {
+            return "Le chien a mangé " + nouriture;
+        }
+        public string Dormir(double secondes)
+        {
+            string[] mesure = ["secondes", "minutes", "heures", "jours", "semaines", "fortnights", "mois", "ans", "décennies", "siècles", "millénaires", "fois l'âge de l'univers"];
+            double[] valeur = [secondes, secondes / 60, secondes / 60 / 60, secondes / 60 / 60 / 24, secondes / 60 / 60 / 24 / 7, secondes / 60 / 60 / 24 / 7 / 2, secondes / 60 / 60 / 24 / 30, secondes / 60 / 60 / 24 / 365, secondes / 60 / 60 / 24 / 365 / 10, secondes / 60 / 60 / 24 / 365 / 100, secondes / 60 / 60 / 24 / 365 / 1000, secondes / 60 / 60 / 24 / 365 / 13800000000];
+            string message = "Le chien a dormi ";
+            for (int iTemps = 0;  iTemps < mesure.Length; iTemps++)
+            {
+                message += valeur[iTemps] + " " + mesure[iTemps];
+                if (iTemps != mesure.Length - 1)
+                message += "\nou ";
+            }
+            return message;
+        }
+        public string AfficherCaracteristiques()
+        {
+            string message = $"Le chien est de race {_race}, son pelage est {_pelage}, il fait {_taille} cm de haut et pèse {_poids} kg, ce qui est moins que {_poids + 1} ";
+            if (_agressivite && _sociabilite)
+            {
+                message += "Il est agressif mais social.";
+            }
+            if (_agressivite && !_sociabilite)
+            {
+                message += "Il est agressif et pas social :(.";
+            }
+            if (!_agressivite && _sociabilite)
+            {
+                message += "Il est non agressif et social :).";
+            }
+            if (!_agressivite && !_sociabilite)
+            {
+                message += "Il n'est rien.";
+            }
+            message += $" Son tag est {_tag} et il a {_age} an";
+            if (_age > 1)
+            {
+                message += "s";
+            }
+            message += ".";
+            return message;
+        }
+    }
+}
